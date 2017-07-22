3da4fb27ded7e2df92b01c61ec305a8901f3c68a
zhaoman <906998248@qq.com>
2017-07-22 16:28:08 +0800

    更新 eureka client

--- springcloud_eureka_client/src/main/java/com/example/springcloud_eureka_client/restful/HelloController.java
+++ springcloud_eureka_client/src/main/java/com/example/springcloud_eureka_client/restful/HelloController.java
@@ -1,12 +1,13 @@
 package com.example.springcloud_eureka_client.restful;
 
+import com.example.springcloud_eureka_client.model.User;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
 
 /**
  * @Author: Leon
@@ -23,10 +24,30 @@ public class HelloController {
     private DiscoveryClient client;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello() {
+    public String hello() throws Exception {
         ServiceInstance serviceInstance = client.getLocalServiceInstance();
+
+        int sleepTime = new Random().nextInt(4000);
+        LOGGER.info("sleepTime: " + sleepTime);
+        Thread.sleep(sleepTime);
+
         LOGGER.info("/hello, host: " + serviceInstance.getHost() + ", service_id: " + serviceInstance.getServiceId());
         return "Hello World";
+    }
+
+    @RequestMapping(value = "/hello1", method = RequestMethod.GET)
+    public String hello(@RequestParam String name) {
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value = "/hello2", method = RequestMethod.GET)
+    public User hello(@RequestHeader String name, @RequestHeader Integer age) {
+        return new User(name, age);
+    }
+
+    @RequestMapping(value = "/hello3", method = RequestMethod.POST)
+    public String hello(@RequestBody User user) {
+        return "Hello " + user.getName() + ", " + user.getAge();
     }
 
 }
